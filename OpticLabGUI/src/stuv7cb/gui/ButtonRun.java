e21a8f341334e55eafa7f9e7402a95943fb4bfb3
Stuv7CB <olev.l.s43@gmail.com>
2015-04-26 17:38:50 +0300

    Working on client

--- OpticLabGUI/src/stuv7cb/gui/ButtonRun.java
+++ OpticLabGUI/src/stuv7cb/gui/ButtonRun.java
@@ -21,6 +21,7 @@ import javax.swing.JTextField;
 
 class ButtonRun extends JButton
 {
+	Socket mainSocket;
 	String ip="ip";
 	boolean saveSocket=false;
 	int port;
@@ -90,14 +91,14 @@ class ButtonRun extends JButton
 			}
 		}
 		MainFrame mainFrame=(MainFrame) c;
-		try
+		/*try
 		{
 			Socket socket=new Socket(ip, port);
 			DataOutputStream out=new DataOutputStream(socket.getOutputStream());
 			DataInputStream in=new DataInputStream(socket.getInputStream());
 			Component[] component=mainFrame.getComponentsofMainPanel();
-			out.write(String.valueOf(component.length).getBytes());
-			out.flush();
+			//out.write(String.valueOf(component.length).getBytes());
+			//out.flush();
 			for (int i=0;i<component.length; i++)
 			{
 				String line=String.valueOf(((LabelObject)component[i]).getID())+" "+((LabelObject)component[i]).getParams();
@@ -105,11 +106,52 @@ class ButtonRun extends JButton
 				out.write(line.getBytes());
 				out.flush();
 			}
+			out.write("end".getBytes());
+			out.flush();
 			while(in.readUTF().equals("Next"))
 			{
 				mainFrame.mainPanelPaint(in.readInt(), in.readInt(), in.readInt(), in.readInt());
 			}
 			socket.close();
+		}
+		catch(ConnectException e)
+		{
+			System.err.println("Can't connect to server");
+		}
+		catch(IOException e)
+		{
+			
+		}*/
+		Component[] component=mainFrame.getComponentsofMainPanel();
+		for (int i=0;i<component.length; i++)
+		{
+			String line=String.valueOf(((LabelObject)component[i]).getID())+" "+((LabelObject)component[i]).getParams();
+			System.out.println(line);
+			send(line);
+		}
+		send("FINISH");
+		try {
+			DataInputStream in=new DataInputStream(mainSocket.getInputStream());
+			int buf=in.read()-48;
+			System.out.println(buf);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	void send(String line)
+	{
+		try
+		{
+			Socket socket=new Socket(ip, port);
+			DataOutputStream out=new DataOutputStream(socket.getOutputStream());
+			DataInputStream in=new DataInputStream(socket.getInputStream());
+			//out.write(String.valueOf(component.length).getBytes());
+			//out.flush();
+				out.write(line.getBytes());
+				out.flush();
+				in.read();
+				mainSocket=socket;
 		}
 		catch(ConnectException e)
 		{
