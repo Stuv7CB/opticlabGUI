83c0794929e8c011a08b6ee0fff3637e35e107cf
Stuv7CB <olev.l.s43@gmail.com>
2015-05-01 22:31:54 +0300

    Remove unused imports.

--- OpticLabGUI/src/stuv7cb/gui/MainPanel.java
+++ OpticLabGUI/src/stuv7cb/gui/MainPanel.java
@@ -3,7 +3,6 @@ package stuv7cb.gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.Locale;
