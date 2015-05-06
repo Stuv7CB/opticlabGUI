999e76cae0dbad5902783bbc9c58e00fafbc51d5
leonid <leonid@localhost.localdomain>
2015-05-06 16:55:03 +0300

    Very minor update: remove useless imports.

--- OpticLabGUI/src/stuv7cb/gui/LabelLense.java
+++ OpticLabGUI/src/stuv7cb/gui/LabelLense.java
@@ -7,7 +7,6 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.geom.Line2D;
-import java.awt.geom.Line2D.Double;
 
 class LabelLense extends LabelObject
 {
